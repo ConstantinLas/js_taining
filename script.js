56c218fa1de8af2a4ccf90a0d15dd29ec060802e
Kos <example@mail.com>
2022-01-05 22:23:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict"; // for modern code ECMAScript 5
+
+let cat = "cars";
+
+console.log(`http://someadres/${cat}`);
+
+console.log(4 + "4");       //-->44
+console.log(4 + + "4");     //-->8
+
+// итерация ++i или i++
+// префиксный и постфиксный
+
+let i = 0,
+    y = 0;
+
+console.log(i++);   // i = 1
+console.log(++y);   // y = 0
+console.log(y);     // y = 1
+
+console.log(5 % 2); // остаток от деления
+
